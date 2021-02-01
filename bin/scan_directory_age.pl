1d4b4ec932a7877d4dc005a81a3f9b23bba2c1dd
Timothy Parnell <parnell.tj@gmail.com>
2021-02-01 15:07:25 -0700

    print total size of the directory in addition to size
    Does this sort of invalidate the name of the script?

--- bin/scan_directory_age.pl
+++ bin/scan_directory_age.pl
@@ -4,7 +4,7 @@
 use strict;
 use File::Find;
 
-our $version = 1.1;
+our $version = 1.2;
 
 ######## Documentation
 my $doc = <<END;
@@ -12,6 +12,7 @@ my $doc = <<END;
 A script to report the ages in days for a give directory.
 It will scan all files in a given directory and sub-directories, 
 and report the age in days from now for the youngest file found.
+It will also generate the total size of the directory.
 
 Version: $version
 
@@ -19,7 +20,7 @@ Usage:
     scan_directory_age.pl [--verbose] /path/to/directory1 ...
 
 Options:
-    --verbose       Report youngest and oldest file
+    --verbose       Report youngest, oldest, and biggest file
     
 END
 
@@ -47,9 +48,12 @@ if (scalar $ARGV[0] =~ /^(?:\-v|\-\-verbose)$/i) {
 # set up
 my $youngest      = 0;
 my $oldest        = 0;
+my $running_size  = 0;
+my $biggest_size  = 0;
 my $youngest_file = '';
 my $oldest_file   = '';
-
+my $biggest_file  = '';
+my $day = 60 * 60 * 24; # in seconds
 
 foreach my $given_dir (@ARGV) {
 	
@@ -58,8 +62,11 @@ foreach my $given_dir (@ARGV) {
 	# (re-)initialize
 	$youngest      = 0;
 	$oldest        = 0;
+	$running_size  = 0;
+	$biggest_size  = 0;
 	$youngest_file = '';
 	$oldest_file   = '';
+	$biggest_file  = '';
 	
 	# search
 	find( {
@@ -74,13 +81,14 @@ foreach my $given_dir (@ARGV) {
 		printf "-\t%s\n", $given_dir;
 		next;
 	}
-	my $day = 60 * 60 * 24;
 	my $youngest_age = (time - $youngest) / $day;
 	my $oldest_age = (time - $oldest) / $day;
-	printf("%0.f\t%s\n", $youngest_age, $given_dir);
+	printf("%.0f\t%s\t%s\n", $youngest_age, generate_short_size($running_size), $given_dir);
 	if ($verbose) {
 		printf("  oldest file at %.0f days is %s\n", $oldest_age, $oldest_file);
 		printf("  youngest file at %.0f days is %s\n", $youngest_age, $youngest_file);
+		printf("  biggest file at %s is %s\n", generate_short_size($biggest_size), 
+			$biggest_file);
 	}
 }
 
@@ -92,15 +100,18 @@ sub age_callback {
 	# skip specific files, including SB preparation files
 	return if -d $file;
 	return if -l $file;
-	return if substr($file,0,1) eq '.'; # dot files are often hidden, backup, or OS stuff
 	return if $file =~ /_MANIFEST\.csv$/; 
 	return if $file =~ /_MANIFEST\.txt$/; 
 	return if $file =~ /_ARCHIVE\.zip$/;
 	return if $file =~ /_ARCHIVE_LIST\.txt$/;
 	return if $file =~ /_REMOVE_LIST\.txt$/;
 	
-	# get age
-	my $age = (stat($file))[9];
+	# get file stats
+	my ($size, $age) = (stat($file))[7,9];
+	$running_size += $size;
+	
+	# dot files are often hidden, backup, or OS stuff - but we still count the size
+	return if substr($file,0,1) eq '.'; 
 	
 	# initialize
 	if ($youngest == 0) {
@@ -109,6 +120,8 @@ sub age_callback {
 		$oldest        = $age;
 		$youngest_file = $file;
 		$oldest_file   = $file;
+		$biggest_file  = $file;
+		$biggest_size  = $size;
 	}
 	
 	# check for younger
@@ -121,6 +134,28 @@ sub age_callback {
 	elsif ($age < $oldest) {
 		$oldest = $age;
 		$oldest_file = $file;
+	}
+	
+	# check file size
+	if ($size > $biggest_size) {
+		$biggest_size = $size;
+		$biggest_file = $file;
+	}
+}
+
+sub generate_short_size {
+	my $size = shift;
+	if ($size > 1000000000) {
+		return sprintf("%.1fG", $size / 1000000000);
+	}
+	elsif ($size > 1000000) {
+		return sprintf("%.1fM", $size / 1000000);
+	}
+	elsif ($size > 1000) {
+		return sprintf("%.1fK", $size / 1000);
+	}
+	else {
+		return sprintf("%dB", $size);
 	}
 }
 
