fc6523f877305e9193e6d6e92ecfc219da79fe8f
Timothy Parnell <parnell.tj@gmail.com>
2026-06-03 16:38:30 -0600

    add new option to post a readme file
    using the put_aws_project_readme script. Update logic to handle either missing manifest or readme files.

--- bin/check_aws_project.pl
+++ bin/check_aws_project.pl
@@ -11,7 +11,7 @@ use FindBin qw($Bin);
 use lib "$Bin/../lib";
 use RepoCatalog;
 
-our $VERSION = 1.1;
+our $VERSION = 1.2;
 
 my $doc = <<END;
 
@@ -25,6 +25,14 @@ Legacy projects in particular may be inaccurate due to initial import
 to SBG, then migration to AWS, and labs that have left or never signed
 up with an account.
 
+It will fix problems if indicated, including uploading a missing manifest
+file and posting a missing readme file. For projects with no corresponding
+bucket found or empty prefixes, it will clear the metadata from the
+Catalog (caution!).
+
+A status is printed for every project, and notifications for every
+missing file uploaded and/or metadata update to the catalog.
+
 Provide a project list of identifiers to check, otherwise it will
 go through every project in the Catalog.
 
@@ -32,15 +40,16 @@ Version: $VERSION
 
 Usage:
 
-  put_aws_project_readme.pl -c Catalog.db  12345R ...
+  check_aws_project.pl -c Catalog.db  12345R ...
 
 Options:
     -c --cat <path>         Path to metadata catalog database. Required.
-    --list <file>           List of project IDs
+    --list <file>           File with list of project IDs
 
 Fixes to problems:
     --update                Update Catalog to remove missing bucket/prefix
     --manifest              Upload a missing manifest file
+    --readme                Post a missing readme file
 
 Other:
     --cred <path>           Path to AWS credentials file. 
@@ -55,6 +64,7 @@ my @project_ids;
 my $list_file;
 my $update = 0;
 my $post_manifest = 0;
+my $post_readme = 0;
 my $aws_cred_file = sprintf "%s/.aws/credentials", $ENV{HOME};
 my $help;
 
@@ -65,6 +75,7 @@ if (@ARGV) {
 		'list=s'                => \$list_file,
 		'update!'               => \$update,
 		'manifest!'             => \$post_manifest,
+		'readme!'               => \$post_readme,
 		'cred=s'                => \$aws_cred_file,
 		'h|help!'               => \$help,
 	) or die " bad options! Please check\n $doc\n";
@@ -179,6 +190,8 @@ sub check_project {
 			}
 		}
 		if ($bucket) {
+
+			# check for manifest
 			my $key    = sprintf "%s/%s_MANIFEST.csv", $Entry->prefix, $id;
 			my $object = $bucket->object(
 				key => $key
@@ -202,6 +215,28 @@ sub check_project {
 					$status = 'manifest not found';
 				}
 			}
+			
+			# check for readme
+			$key    = sprintf "%s/%s_README.txt", $Entry->prefix, $id;
+			$object = $bucket->object(
+				key => $key
+			);
+			undef $e;
+			eval {$e = $object->exists};
+			if ($e) {
+				if ( $status ne 'ok' ) {
+					$status .= '; readme found';
+				}
+				# otherwise it's ok
+			}
+			else {
+				if ( $status eq 'ok' ) {
+					$status = 'readme not found';
+				}
+				else {
+					$status .= '; readme not found';
+				}
+			}
 		}
 		else {
 			$status = 'bucket not found';
@@ -212,57 +247,82 @@ sub check_project {
 	
 	# update
 	if ( $update and $status eq 'bucket not found' ) {
+		my $text = sprintf
+			" ! Removed bucket/prefix metadata '%s/%s' from Catalog for %s\n",
+			$Entry->bucket, $Entry->prefix, $id;
 		$Entry->bucket( q() );
 		$Entry->prefix( q() );
-		printf " ! Removed bucket/prefix metadata from Catalog for %s\n", $id;
+		print $text;
 	}
-	elsif ( $post_manifest and $status eq 'manifest not found' ) {
+	elsif ( $status =~ /not found/ and ( $post_manifest or $post_readme ) ) {
 		# first check that there are contents in the bucket
 		my $prefix = $Entry->prefix;
 		my $stream = $bucket->list( { prefix => $prefix } );
 		my $count = 0;
-		while ( my $object = $stream->items ) {
+		foreach my $object ( $stream->items ) {
+			next if $object->key =~ m|/$|; # skip folders
 			$count++;
 			last if $count > 2; # good enough
 		}
-		if ($count) {
+		if ($count >= 2) {
 			# folder is not empty
-			my $destination;
-			my $source = sprintf "%s/%s_MANIFEST.csv", $Entry->path, $id;
-			if ( -e $source ) {
-				$destination = sprintf "s3://%s/%s/%s_MANIFEST.csv", $Entry->bucket,
-					$Entry->prefix, $id;
-			}
-			else {
-				$source = sprintf "%s/%s_MANIFEST.txt", $Entry->path, $id;
+			
+			# post a missing manifest
+			if ( $post_manifest and $status =~ /manifest \s not \s found/x ) {
+				my $destination;
+				my $source = sprintf "%s/%s_MANIFEST.csv", $Entry->path, $id;
 				if ( -e $source ) {
-					$destination = sprintf "s3://%s/%s/%s_MANIFEST.txt", $Entry->bucket,
+					$destination = sprintf "s3://%s/%s/%s_MANIFEST.csv", $Entry->bucket,
 						$Entry->prefix, $id;
 				}
 				else {
-					printf " ! No source manifest file available in %s, not uploading\n",
-						$Entry->path; 
+					$source = sprintf "%s/%s_MANIFEST.txt", $Entry->path, $id;
+					if ( -e $source ) {
+						$destination = sprintf "s3://%s/%s/%s_MANIFEST.txt",
+							$Entry->bucket, $Entry->prefix, $id;
+					}
+					else {
+						printf " ! No source manifest file available in %s, not uploading\n",
+							$Entry->path; 
+					}
+				}
+	
+				# we have a source and destination
+				if ($destination) {
+					my $cmd = sprintf "aws s3 cp %s %s --profile %s --no-progress", $source,
+						$destination, $Entry->profile;
+					# printf " > Executing %s\n", $cmd;
+					my $result = qx($cmd);
+					chomp $result;
+					if ($result =~ m|\A upload: \s (?:[\.\/\w]+)? $source \s to \s $destination|x) {
+						printf " > Successfully uploaded %s\n", $destination;
+					}
+					else {
+						printf " ! Error: %s\n", $result;
+					}
 				}
 			}
-
-			# we have a source and destination
-			if ($destination) {
-				my $cmd = sprintf "aws s3 cp %s %s --profile %s --no-progress", $source,
-					$destination, $Entry->profile;
-				# printf " > Executing %s\n", $cmd;
+			
+			# post a missing readme
+			if ( $post_readme and $status =~ /readme \s not \s found/x ) {
+				# for this we call another program
+				my $cmd = sprintf "%s/put_aws_project_readme.pl -c %s -p %s", $Bin,
+					$cat_file, $id;
 				my $result = qx($cmd);
-				chomp $result;
-				if ($result =~ m|\A upload: \s (?:[\.\/\w]+)? $source \s to \s $destination|x) {
-					printf " > Successfully uploaded %s\n", $destination;
-				}
-				else {
-					printf " ! Error: %s\n", $result;
-				}
+				print $result;
 			}
 		}
 		else {
-			printf " ! Prefix %s/%s is empty, not uploading manifest\n", $Entry->bucket,
-				$Entry->prefix;
+			printf " ! Prefix %s/%s appears empty, not uploading manifest or readme\n",
+				$Entry->bucket, $Entry->prefix;
+			if ($update) {
+				my $text = sprintf
+					" ! Removed bucket/prefix metadata '%s/%s' from Catalog for %s\n",
+					$Entry->bucket, $Entry->prefix, $id;
+				$Entry->bucket( q() );
+				$Entry->prefix( q() );
+				print $text;
+			}
 		}
 	}
 }
